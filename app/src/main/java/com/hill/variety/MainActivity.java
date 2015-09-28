b68743b2c4fca3f6c6167d47c84c2c18e21400ff
Erko Teor <erkoteor@hotmail.com>
2015-09-28 20:34:24 +0300

    Minor detailing on the login view

--- app/src/main/java/com/hill/variety/MainActivity.java
+++ app/src/main/java/com/hill/variety/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void setFacebookLoginCallbackManager() {
         callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.facebookLoginButton);
         loginButton.registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
