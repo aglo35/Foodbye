dfbfe29205b361cd5fc62e559a723f5b0bc7211c
aglo35 <allar.viinamae@hotmail.com>
2015-09-28 19:08:32 +0300

    Added sign up xml

---
+++ app/src/main/java/com/hill/variety/SignUpActivity.java
@@ -0,0 +1,11 @@
+package com.hill.variety;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ * Created by allar on 28.09.15.
+ */
+public class SignUpActivity extends AppCompatActivity {
+
+}
