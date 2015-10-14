8ed7e9717f678c7a097a79a82d150b9e8ec6e2bd
kristiina.voolar <kristiinavoolar@gmail.com>
2015-10-14 12:31:34 +0300

    TEST - kas koostisosad on null-id

---
+++ app/src/androidTest/java/com/hill/variety/model/RecipeTest.java
@@ -0,0 +1,15 @@
+package com.hill.variety.model;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Kristiina on 14/10/2015.
+ */
+public class RecipeTest extends TestCase {
+
+    public void testGetIngredientsForDisplay() throws Exception {
+        Recipe recipe = new Recipe();
+        assertNotNull(recipe);
+        assertTrue(recipe != null);
+    }
+}
